6e1f255a3eccaf5a6f89b0536b55097b3c8a4268
Andrey <31798016+stryukovap@users.noreply.github.com>
2019-07-21 18:39:15 +0300

    cleaned project

--- layouts/error.vue
+++ layouts/error.vue
@@ -23,7 +23,7 @@
     },
     head() {
       const title =
-        this.error.statusCode === 404 ? this.pageNotFound : this.otherError
+        this.error.statusCode === 404 ? this.pageNotFound : this.otherError;
       return {
         title
       }

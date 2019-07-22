bc0e480e33c1e437f125347cb6eca1e69063401c
stryukovap <kitten1981>
2019-07-22 10:31:53 +0300

    same refactor card

--- components/Card.vue
+++ components/Card.vue
@@ -1,29 +1,106 @@
 <template>
-  <li>
-    <a href="#" @click.prevent="goTo(id)">
-      {{id}} {{name}}
-      {{height}} {{weight}} {{base_experience}} {{front_default}} {{back_default}}
-      <img :src="front_default" alt="front_default">
-      <img :src="back_default" alt="back_default">
-    </a>
-  </li>
+    <v-flex xs12>
+        <v-card @click.prevent="goTo(id)">
+            <v-layout>
+                <v-flex xs4>
+                    <div class="flip-card">
+                        <div class="flip-card-inner">
+                            <div class="flip-card-front">
+                                <v-img class="flip-card-img" :src="front_default" alt="front_default"></v-img>
+                            </div>
+                            <div class="flip-card-back">
+                                <v-img class="flip-card-img" :src="back_default" alt="front_default"></v-img>
+                            </div>
+                        </div>
+                    </div>
+                </v-flex>
+                <v-flex xs8>
+                    <div>
+                        <v-card-title primary-title>
+                            {{name}}
+                        </v-card-title>
+                        <div>
+                            height {{height}}
+                        </div>
+                        <div>
+                            weight {{weight}}
+                        </div>
+                        <div>
+                            base_experience {{base_experience}}
+                        </div>
+                    </div>
+                </v-flex>
+            </v-layout>
+        </v-card>
+    </v-flex>
 </template>
-<script>
-  export default {
-    name: 'card',
-    props: {
-      name: String,
-      height: Number,
-      weight: Number,
-      base_experience: Number,
-      id: Number,
-      front_default: String,
-      back_default: String
-    },
-    methods: {
-      goTo(id) {
-        this.$router.push('/pokemon/' + id)
-      }
+<style scoped>
+    .v-card:hover {
+        cursor: pointer;
     }
-  }
+
+    .flip-card-img {
+        width: auto;
+        height: 150px;
+    }
+
+    .flip-card {
+        width: 150px;
+        height: 150px;
+        background-color: transparent;
+        perspective: 1000px; /* Remove this if you don't want the 3D effect */
+    }
+
+    /* This container is needed to position the front and back side */
+    .flip-card-inner {
+        position: relative;
+        width: 100%;
+        height: 100%;
+        text-align: center;
+        transition: transform 0.8s;
+        transform-style: preserve-3d;
+    }
+
+    /* Do an horizontal flip when you move the mouse over the flip box container */
+    .flip-card:hover .flip-card-inner {
+        transform: rotateY(180deg);
+    }
+
+    /* Position the front and back side */
+    .flip-card-front, .flip-card-back {
+        position: absolute;
+        width: 100%;
+        height: 100%;
+        backface-visibility: hidden;
+    }
+
+    /* Style the front side (fallback if image is missing) */
+    .flip-card-front {
+        background-color: transparent;
+    }
+
+    /* Style the back side */
+    .flip-card-back {
+        background-color: transparent;
+        transform: rotateY(180deg);
+    }
+</style>
+<script>
+    export default {
+        name: 'card',
+        props: {
+            name: String,
+            height: Number,
+            weight: Number,
+            base_experience: Number,
+            id: Number,
+            front_default: String,
+            back_default: String
+        },
+        methods: {
+            goTo(id) {
+                this.$router.push('/pokemon/' + id)
+            }
+        }
+    }
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,44 +1,54 @@
 <template>
-  <div>
-    <ul>
-      <!--      <Card v-for="(item,index) in list" :key="index"/>-->
-      <Card v-for="(item,index) in list" :key="index"
-            :height="item.height"
-            :weight="item.weight"
-            :base_experience="item.base_experience"
-            :id="item.id"
-            :name="item.name"
-            :front_default="item.sprites.front_default"
-            :back_default="item.sprites.back_default"/>
-    </ul>
-  </div>
+    <div>
+        <!--        <ul>-->
+        <!--      <Card v-for="(item,index) in list" :key="index"/>-->
+        <!--      <Card v-for="(item,index) in list" :key="index"-->
+        <!--            :height="item.height"-->
+        <!--            :weight="item.weight"-->
+        <!--            :base_experience="item.base_experience"-->
+        <!--            :id="item.id"-->
+        <!--            :name="item.name"-->
+        <!--            :front_default="item.sprites.front_default"-->
+        <!--            :back_default="item.sprites.back_default"/>-->
+        <v-list>
+            <Card v-for="(item,index) in list" :key="index"
+                  :height="item.height"
+                  :weight="item.weight"
+                  :base_experience="item.base_experience"
+                  :id="item.id"
+                  :name="item.name"
+                  :front_default="item.sprites.front_default"
+                  :back_default="item.sprites.back_default"/>
+        </v-list>
+        <!--        </ul>-->
+    </div>
 </template>
 
 <script>
 
 
-  import Card from "../components/Card";
-  import {mapActions, mapState} from 'vuex';
+    import Card from "../components/Card";
+    import {mapActions, mapState} from 'vuex';
 
-  export default {
-    components: {Card},
-    data() {
-      return {}
-    },
-    async fetch({store, params}) {
-      console.log('fetch');
-      await store.dispatch('getList', {root: true});
-    },
-    computed:
-      mapState([
-        'list'
-      ])
-    ,
-    methods: {
-      ...mapActions([
-        'getList'
-      ])
+    export default {
+        components: {Card},
+        data() {
+            return {}
+        },
+        async fetch({store, params}) {
+            console.log('fetch');
+            await store.dispatch('getList', {root: true});
+        },
+        computed:
+            mapState([
+                'list'
+            ])
+        ,
+        methods: {
+            ...mapActions([
+                'getList'
+            ])
+        }
+
     }
-
-  }
 </script>
